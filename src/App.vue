b96f7180cfa4bf10bde46aa3deee95185d019557
Taiki Tanaka <11ta7ka@gmail.com>
2020-11-20 00:18:33 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<navigation-header />
+<router-view />
+</template>
+
+<script lang="ts">
+import {
+  defineComponent
+} from "vue";
+import navigationHeader from "@/components/navigationHeader.vue"; // @ is an alias to /src
+
+export default defineComponent({
+  name: "App",
+  components: {
+    navigationHeader,
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
