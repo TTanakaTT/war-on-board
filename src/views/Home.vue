6efa5b942a7996dc7f372855fd525646f35ac4b8
Taiki Tanaka <11ta7ka@gmail.com>
2021-12-05 01:06:14 +0900

    add input element for layer number

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="home"></div>
-  <hexagon-panels :layer="4" />
+  <div class="home">
+    <hexagon-panels :layer="layer" />
+    <input type="number" id="layer" v-model="layer" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,5 +11,11 @@ import HexagonPanels from "@/components/HexagonPanels.vue";
 
 export default defineComponent({
   components: { HexagonPanels },
+  data() {
+    const layer = 1;
+    return {
+      layer,
+    };
+  },
 });
 </script>
