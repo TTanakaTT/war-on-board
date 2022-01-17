5c1d64b610bcb6104e13e8fd43148f007d54422c
Taiki Tanaka <11ta7ka@gmail.com>
2022-01-17 21:43:43 +0900

    use class path

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
-import vuetify from "./plugins/vuetify";
-import { loadFonts } from "./plugins/webfontloader";
+import App from "@/App.vue";
+import "@/registerServiceWorker";
+import router from "@/router";
+import store from "@/store";
+import vuetify from "@/plugins/vuetify";
+import { loadFonts } from "@/plugins/webfontloader";
 
 loadFonts();
 
