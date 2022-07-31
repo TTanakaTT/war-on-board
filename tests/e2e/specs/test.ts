fc9ff45614794475d6882762e61388e9c0cf224b
Taiki Tanaka <11ta7ka@gmail.com>
2022-08-01 02:38:54 +0900

    update e2e test

--- tests/e2e/specs/test.ts
+++ tests/e2e/specs/test.ts
@@ -3,12 +3,11 @@
 describe("My First Test", () => {
   it("Visits the app root url", () => {
     cy.visit("/");
-    cy.get(".v-toolbar").find(".mdi-menu").click();
     cy.get(".v-navigation-drawer").contains(
       ".v-list-item-title",
       "War-on-Board"
     );
-    cy.get(".v-navigation-drawer__scrim").click("right");
+    cy.get(".v-toolbar").find(".mdi-menu").click();
 
     cy.get("#app").find("#layer").should("have.value", 5);
     cy.get(".layered-hexagon-panels > .horizontal-layer").should(
@@ -33,23 +32,32 @@ describe("My First Test", () => {
     cy.get(
       "#inspire > div > main > div > div > div > div > div:nth-child(3) > div:nth-child(1) > div"
     ).as("panel-2-1");
+    cy.get(
+      "#inspire > div > main > div > div > div > div > div:nth-child(4) > div:nth-child(1) > div"
+    ).as("panel-3-1");
 
     cy.get("@panel-1-1").should("have.class", "occupied");
+    cy.get("@panel-2-1").should("have.class", "unoccupied");
+    cy.get("@panel-3-1").should("have.class", "unoccupied");
 
     cy.get("@panel-1-1").click();
     cy.get("@panel-1-1").should("have.class", "selected");
     cy.get("@panel-2-1").should("have.class", "movable");
+    cy.get("@panel-3-1").should("have.class", "immovable");
 
     cy.get("@panel-2-1").click();
-    cy.get("@panel-2-1").should("have.class", "occupied");
     cy.get("@panel-1-1").should("have.class", "unoccupied");
+    cy.get("@panel-2-1").should("have.class", "occupied");
+    cy.get("@panel-3-1").should("have.class", "unoccupied");
 
     cy.get("@panel-2-1").click();
-    cy.get("@panel-2-1").should("have.class", "selected");
     cy.get("@panel-1-1").should("have.class", "movable");
+    cy.get("@panel-2-1").should("have.class", "selected");
+    cy.get("@panel-3-1").should("have.class", "movable");
 
     cy.get("@panel-1-1").click();
     cy.get("@panel-1-1").should("have.class", "occupied");
     cy.get("@panel-2-1").should("have.class", "unoccupied");
+    cy.get("@panel-3-1").should("have.class", "unoccupied");
   });
 });
