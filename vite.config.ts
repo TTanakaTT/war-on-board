fd8e31abf1f5871d23026b09306c0beffff91fcb
Taiki Tanaka <11ta7ka@gmail.com>
2025-06-10 02:05:18 +0900

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
+		devtoolsJson(),
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
